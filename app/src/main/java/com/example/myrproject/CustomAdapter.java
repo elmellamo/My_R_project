db656da916657ce340e73942c9a9a5fa67116119
DongGi-create <oz9911@naver.com>
2022-05-24 20:41:00 +0900

    db

--- app/src/main/java/com/example/myrproject/CustomAdapter.java
+++ app/src/main/java/com/example/myrproject/CustomAdapter.java
@@ -28,7 +28,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
     //생성자 Alt + insert control+a enter
 
 
-    public CustomAdapter(ArrayList<refrigeitem> refrigeiatems, Context mContext) {
+    public CustomAdapter(ArrayList<refrigeitem> refrigeitems, Context mContext) {
         this.mrefrigeitem = refrigeitems;
         this.mContext = mContext;
         mDBHelper = new DBHelper(mContext);
@@ -52,13 +52,14 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
 
     @Override
     public int getItemCount() {
-        return mTodoItems.size();
+        return mrefrigeitem.size();
     }//recycler view
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         private TextView tv_title;
         private TextView tv_content;
         private TextView tv_writeDate;
+        private Button edit_btn;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -66,69 +67,49 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
             tv_title = itemView.findViewById(R.id.tv_title);
             tv_content = itemView.findViewById(R.id.tv_content);
             tv_writeDate = itemView.findViewById(R.id.tv_date);
+            edit_btn = itemView.findViewById(R.id.edit_btn);
 
-            itemView.setOnClickListener(new View.OnClickListener() {
+            edit_btn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     int curPos = getAdapterPosition();// 현재 리스트 클릭한 아이템위치
-                    TodoItem todoItem = mTodoItems.get(curPos);//아이템 정보 가져온다
+                    refrigeitem todoItem = mrefrigeitem.get(curPos);//아이템 정보 가져온다
+                    Dialog dialog = new Dialog(mContext, android.R.style.Theme_Material_Light_Dialog);
+                    dialog.setContentView(R.layout.dialog_edit);//뷰가 연결되었으므로 이 레이아웃에서 find view by id사용 가능
+                    EditText et_title = dialog.findViewById(R.id.et_title);//그냥 find가 아니라 dialog.~해야 한다
+                    EditText et_content = dialog.findViewById(R.id.et_content);
+                    Button btn_ok = dialog.findViewById(R.id.btn_ok);
 
-                    String[] strChoiceItems = {"수정하기","삭제하기"};
-                    AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
-                    builder.setTitle("원하는 작업을 선택해주세요");
-                    builder.setItems(strChoiceItems, new DialogInterface.OnClickListener() {
+                    et_title.setText(todoItem.getTitle());
+                    et_content.setText(todoItem.getContent());
+                    et_title.setSelection(et_title.getText().length());
+
+
+                    btn_ok.setOnClickListener(new View.OnClickListener() {
                         @Override
-                        public void onClick(DialogInterface dialogInterface, int position) {//position을 의미
-                            if(position == 0) {
-                                // 수정하기
-                                Dialog dialog = new Dialog(mContext, android.R.style.Theme_Material_Light_Dialog);
-                                dialog.setContentView(R.layout.dialog_edit);//뷰가 연결되었으므로 이 레이아웃에서 find view by id사용 가능
-                                EditText et_title = dialog.findViewById(R.id.et_title);//그냥 find가 아니라 dialog.~해야 한다
-                                EditText et_content = dialog.findViewById(R.id.et_content);
-                                Button btn_ok = dialog.findViewById(R.id.btn_ok);
+                        public void onClick(View view) {
+                            //수정시나리오
+                            //update table
+                            String title = et_title.getText().toString();
+                            String content = et_content.getText().toString();
+                            String currentTime = new SimpleDateFormat("yyyy_MM_dd HH:mm:ss").format(new Date());//현재 시간 연월일시분초 받아오기
+                            String beforeTime = todoItem.getWriteDate();//이전에 저장된 시간
 
-                                et_title.setText(todoItem.getTitle());
-                                et_content.setText(todoItem.getContent());
-                                et_title.setSelection(et_title.getText().length());
+                            mDBHelper.UpdateTodo(title, content, currentTime, beforeTime);//입력필드에 적은 값 가져온다
+                            //UpdateTodo ctrl누르면서 클릭하면 그 함수로 이동할 수 있다
 
+                            //update UI
+                            todoItem.setTitle(title);
+                            todoItem.setContent(content);
+                            todoItem.setWriteDate(currentTime);
+                            notifyItemChanged(curPos, todoItem);//클릭한 아이템에 갱신된 아이템을 갱신
+                            dialog.dismiss();//dialog 종료
+                            Toast.makeText(mContext, "목록 수정이 완료 되었습니다.", Toast.LENGTH_SHORT).show();
 
-                                btn_ok.setOnClickListener(new View.OnClickListener() {
-                                    @Override
-                                    public void onClick(View view) {
-                                        //수정시나리오
-                                        //update table
-                                        String title = et_title.getText().toString();
-                                        String content = et_content.getText().toString();
-                                        String currentTime = new SimpleDateFormat("yyyy_MM_dd HH:mm:ss").format(new Date());//현재 시간 연월일시분초 받아오기
-                                        String beforeTime = todoItem.getWriteDate();//이전에 저장된 시간
-
-                                        mDBHelper.UpdateTodo(title, content, currentTime,beforeTime);//입력필드에 적은 값 가져온다
-                                        //UpdateTodo ctrl누르면서 클릭하면 그 함수로 이동할 수 있다
-
-                                        //update UI
-                                        todoItem.setTitle(title);
-                                        todoItem.setContent(content);
-                                        todoItem.setWriteDate(currentTime);
-                                        notifyItemChanged(curPos, todoItem);//클릭한 아이템에 갱신된 아이템을 갱신
-                                        dialog.dismiss();//dialog 종료
-                                        Toast.makeText(mContext, "목록 수정이 완료 되었습니다.", Toast.LENGTH_SHORT).show();
-
-                                    }
-                                });
-                                dialog.show();//필수
-
-                            }else if(position == 1){
-                                // delete table
-                                String beforeTime = todoItem.getWriteDate();
-                                mDBHelper.deleteTodo(beforeTime);
-                                // delete UI
-                                mTodoItems.remove(curPos);
-                                notifyItemRemoved(curPos);
-                                Toast.makeText(mContext, "목록이 제거 되었습니다", Toast.LENGTH_SHORT).show();
-                            }
                         }
                     });
-                    builder.show();
+                    dialog.show();//필수
+
                 }
             });
         }
