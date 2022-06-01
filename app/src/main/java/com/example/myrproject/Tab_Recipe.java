25f7ad700888de3b050590840945ee980f74f12a
elmellamo <th6986@naver.com>
2022-06-02 01:45:44 +0900

    dk......다이얼로그,...

--- app/src/main/java/com/example/myrproject/Tab_Recipe.java
+++ app/src/main/java/com/example/myrproject/Tab_Recipe.java
@@ -1,5 +1,6 @@
 package com.example.myrproject;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
@@ -17,6 +18,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -55,8 +58,25 @@ public class Tab_Recipe extends Fragment {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), AddRecipe.class);
-                startActivity(intent);
+                Dialog dialog = new Dialog(getContext(), android.R.style.Theme_Material_Light_Dialog);
+                dialog.setContentView(R.layout.dialog_recipe);
+
+                EditText et_name = dialog.findViewById(R.id.et_name);
+                //여기 et_name이 RecipeDB에 들어갈 수 있도록 저장하는 거 필요 >> 일단 좀 있따가
+                Button btn_add = dialog.findViewById(R.id.btn_add);
+
+                btn_add.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                        Intent intent = new Intent(getActivity(), AddRecipe.class);
+                        startActivity(intent);
+
+                    }
+                });
+
+
+
             }
         });
         return rootview;
