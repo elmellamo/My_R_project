31d5c231eb7188f7c077d7758969d8301429b251
mywoospam <dobbydavid1@naver.com>
2022-06-01 12:18:09 +0900

    카테고리 추가완료

--- app/src/main/java/com/example/myrproject/Fruit.java
+++ app/src/main/java/com/example/myrproject/Fruit.java
@@ -41,8 +41,21 @@ public class Fruit extends Fragment {
         LayoutManager = new GridLayoutManager(getActivity(), 5);
         todo.setLayoutManager(LayoutManager);
 
-        String[] textSet = {"사과", "배","귤","감","키위","토마토","자두","복숭아","포도","수박","체리","바나나","파인애플","오렌지","자몽","레몬","라임","망고","아보카도","땅콩","호두"};
-        int[] imgSet = {R.drawable.apple, R.drawable.pear,R.drawable.tangerine,R.drawable.persimmon,R.drawable.kiwi,R.drawable.tomato,R.drawable.plum,R.drawable.peach,R.drawable.grape,R.drawable.watermelon,R.drawable.cherry,R.drawable.banana,R.drawable.pineapple,R.drawable.mandarin,R.drawable.grapefruit,R.drawable.lemon,R.drawable.lime,R.drawable.mango,R.drawable.avocado,R.drawable.peanut,R.drawable.walnut};
+        String[] textSet = {"사과", "배","귤","감","키위","토마토","자두","복숭아","포도"
+                ,"오디","라즈베리","딸기","블루베리","수박","체리","바나나","파인애플","오렌지","자몽"
+                ,"레몬","라임","깔라만시","망고","망고스틴","용과","패션후르츠","참외","석류"
+                ,"코코넛","두리안","아보카도","땅콩"
+                ,"호두","캐슈넛"};
+        int[] imgSet = {R.drawable.apple, R.drawable.pear,R.drawable.tangerine,R.drawable.persimmon
+                ,R.drawable.kiwi,R.drawable.tomato,R.drawable.plum,R.drawable.peach,R.drawable.grape
+                ,R.drawable.mulberry,R.drawable.rasberry,R.drawable.strawberry,R.drawable.blueberries
+                ,R.drawable.watermelon
+                ,R.drawable.cherry,R.drawable.banana,R.drawable.pineapple,R.drawable.mandarin
+                ,R.drawable.grapefruit,R.drawable.lemon,R.drawable.lime,R.drawable.calamansi
+                ,R.drawable.mango,R.drawable.mangosteen,R.drawable.dragonfruit,R.drawable.passionfruit
+                ,R.drawable.koreanmelon,R.drawable.pomegranate,R.drawable.coconut,R.drawable.durian
+                ,R.drawable.avocado,R.drawable.peanut,R.drawable.walnut
+                ,R.drawable.cashewnut};
 
         //어댑터 연결하기
         Myadapter mAdapter = new Myadapter(imgSet, textSet, getActivity().getApplicationContext());
